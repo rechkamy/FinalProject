bb551e3e02f56561b408ab96bb7da5cbfdc4809f
rechkamy <rechkamy@umich.edu>
2018-04-11 13:04:56 -0400

    Defined functions for use

--- rocchio.py
+++ rocchio.py
@@ -3,7 +3,20 @@ import os
 import sys
 import operator
 
+def trainRocchio:
+
+  
+def testRocchio:
+  
+  
+
 def main():
+  filename = argv[1]
+  tweets = open(filename, 'r').read().split('\n')
+  length = len(tweets)
+  
+  
+  
 
 if __name__ == '__main__':
     main()
